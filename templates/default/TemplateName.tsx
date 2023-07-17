dcfe2db56dea30f578d6ea8c5f5095be8461a561
Maxim Bernatsky <maxsateon@yandex.ru>
2023-07-17 03:10:50 +0300

    feat: create header component with theme switcher

--- templates/default/TemplateName.tsx
+++ templates/default/TemplateName.tsx
@@ -1,9 +1,5 @@
-import styles from './TemplateName.module.scss';
+interface TemplateNameProps {}
 
-interface TemplateNameProps { }
-
-export const TemplateName = ({ }: TemplateNameProps) => (
-  <div className={styles.TemplateName} data-testid="TemplateName">
-    TemplateName Component
-  </div>
+export const TemplateName = ({}: TemplateNameProps) => (
+  <div>TemplateName Component</div>
 );
